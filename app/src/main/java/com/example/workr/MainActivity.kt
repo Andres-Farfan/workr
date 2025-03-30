ffe4b300bd23fa39aab557979033b84e488ed98d
Andres Farfan <a22310560@ceti.mx>
2025-03-30 14:50:18 -0600

    Cambiada solicitud web a ejecución no-bloqueante

--- app/src/main/java/com/example/workr/MainActivity.kt
+++ app/src/main/java/com/example/workr/MainActivity.kt
@@ -6,15 +6,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.request.get
 import io.ktor.client.statement.HttpResponse
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var txtResponse: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -25,29 +27,35 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val txtResponse: TextView = findViewById(R.id.txtResponse)
+        txtResponse = findViewById(R.id.txtResponse)
+        txtResponse.text = "Loading request..."
 
-        // Se configuran el cliente http de Ktor y la url obtenida desde propiedades.
+        // Se lanza una corutina con la tarea de hacer la solicitud web
+        // usando un Scope de corutinas no-bloqueante al nivel del ciclo de vida de la Activity.
+        lifecycleScope.launch { makeRequest() }
+    }
+
+    /**
+     * Hace una solicitud HTTP y muestra el resultado con texto en la vista.
+     */
+    suspend fun makeRequest() {
+        // Configuración del cliente http y la URL obtenida desde propiedades.
         val client = HttpClient(CIO)
         val url = BuildConfig.CONNECTION_TEST_URL
 
-        // Creación de una corutina de kotlin para hacer una solicitud web (actividad asíncrona).
-        runBlocking {
-            launch {
-                try {
-                    // Con una solicitud correcta se mostrarán los datos de la respuesta en un TextView.
-                    val response: HttpResponse = client.get(url)
-                    txtResponse.text = "URL: $url\nHttp response status:\n${response.status}"
-                }
-                // Manejo de excepciones de conectividad.
-                catch (e: Exception) {
-                    e.printStackTrace()
-                }
-                finally {
-                    // Liberación de recursos del ktor client.
-                    client.close()
-                }
-            }
+        try {
+            // Con una solicitud correcta se mostrarán los datos de la respuesta en un TextView.
+            val response: HttpResponse = client.get(url)
+            txtResponse.text = "URL: $url\nHttp response status:\n${response.status}"
+        }
+        // Manejo de excepciones de conectividad.
+        catch (e: Exception) {
+            e.printStackTrace()
+        }
+        finally {
+            // Liberación de recursos del ktor client.
+            client.close()
+            println("Ktor client closed")
         }
     }
 }
