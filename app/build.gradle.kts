5837d9972e15032b6d51c6496ba74397f1c71d06
DarkMagic48 <119092754+DarkMagic48@users.noreply.github.com>
2025-04-05 15:37:24 -0600

    Interfaces Flujo de Registro Orientado a Usuarios
    Se agregan las interfaces de Registro de Usuario, Completar perfil de usuario y la Vista de perfil de usuario, agregando varios de los colores de la paleta a colors.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21"
 }
 
 android {
@@ -17,6 +18,24 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    buildFeatures {
+        compose = true
+    }
+
+    //composeOptions {
+        // Compatible con Kotlin 2.0.21
+       // kotlinCompilerExtensionVersion = "1.6.7"
+    //}
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -26,22 +45,25 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.ui.unit.android)
+    implementation(libs.androidx.ui.tooling.preview.android)
+
+    // Compose (1.6.7 es compatible con Kotlin 2.0.21)
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.compose.ui:ui:1.6.7")
+    implementation("androidx.compose.material:material:1.6.7")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.6.7")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    debugImplementation("androidx.compose.ui:ui-tooling-preview:1.6.7")
+    // Tests
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
