b72fb40a356d448df38d26d1c0c9f96b79457155
DarkMagic48 <119092754+DarkMagic48@users.noreply.github.com>
2025-05-21 19:07:29 -0600

    Pantalla de Oficina Virtual
    Se agrega la pantalla de Oficina Virtual pendiente de contenido, aun falta implementar la navegacion con el nuevo sistema de la API

---
+++ app/src/main/java/com/example/workr/VirtualOfficeScreen.kt
@@ -0,0 +1,39 @@
+package com.example.workr
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+
+@Composable
+fun  VirtualOfficeScreen(navController: NavHostController, isEmpleado : Boolean){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        // Barra azul con la barra de navegación
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp)
+                .background(Color(0xFF007AFF)),
+            contentAlignment = Alignment.CenterStart
+        ) {
+            WorkRTopBar(navController = navController, isEmpleado = isEmpleado)
+        }
+
+        // Contenido vacío por ahora
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+        ) {
+
+        }
+    }
+}
